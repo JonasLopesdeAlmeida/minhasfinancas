978e00a329d728e0be2f768aaa9c149653b12824
Jonas Lopes <jmdlopes.almeida@gmail.com>
2020-04-25 20:26:15 -0300

    Fazendo mais testes para UsuarioRepository

--- src/test/java/com/br/minhasfinancas/repositories/UsuarioRepositoryTest.java
+++ src/test/java/com/br/minhasfinancas/repositories/UsuarioRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.br.minhasfinancas.repositories;
 
+import java.util.Optional;
+
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,12 +40,12 @@ public class UsuarioRepositoryTest {
 		// eu vou precisar de tres elementos: cenário, ação/execução e verificação.
 
 		// cenario
-	    Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
+	    Usuario usuario = criarUsuario();
 		entityManager.persist(usuario);
 		
 		
 		//acao
-		boolean resultado = repo.existsByEmail("usuario@email.com");
+		boolean resultado = repo.existsByEmail("jonas@gmail.com");
 
 	    //verificacao
 		Assertions.assertThat(resultado).isTrue();
@@ -58,9 +60,68 @@ public class UsuarioRepositoryTest {
 		//eu não preciso mais do cenario de deletar pq o @DataJpaTest já sobreescreve o banco.
 		
 		//ação
-		boolean resultado = repo.existsByEmail("usuario@email.com");
+		boolean resultado = repo.existsByEmail("jonas@gmail.com");
 	
 		//verificacao
 		Assertions.assertThat(resultado).isFalse();
 	}
+	
+	@Test
+	public void devePersistirUmUsuarioNaBaseDeDados() {
+		
+		//cenario
+		//aqui ele não recebe o atributo Id pq ele ainda não foi salvo no banco.
+		Usuario usuario =criarUsuario();	
+		//ação
+		//nesse momento depois de salvo ele tem um Id.
+		Usuario usuarioSalvo = repo.save(usuario);
+		
+		//verificacao
+		//verifca pra mim se esse usuario que eu estou mandando existe no banco.
+		Assertions.assertThat(usuarioSalvo.getId()).isNotNull();
+	}
+	
+	//testando metodo que busca um usuario por email.
+	
+	@Test
+	public void deveBuscarUmUsuarioPorEmail() {
+		
+		//cenario
+		Usuario usuario = criarUsuario();
+		entityManager.persist(usuario);
+		
+		
+		//ação
+		
+		//verificacao
+		Optional<Usuario> resultado = repo.findByEmail("jonas@gmail.com");
+		//se o retultadoestiver presente no meu optional ele deve retornar True.
+		Assertions.assertThat(resultado.isPresent()).isTrue();
+	}
+	
+	
+	@Test
+	public void deveRetornarVazioAoBuscarUmUsuarioPorEmailQuandoNaoExisteNoBanco() {
+				
+		//ação
+		//aqui eu nao salva um usuario para poder verificar.
+		
+		//verificacao
+		Optional<Usuario> resultado = repo.findByEmail("jonas@gmail.com");
+		//se o retultado não estiver presente no meu banci ele deve retornar falso.
+		Assertions.assertThat(resultado.isPresent()).isFalse();
+	}
+	
+	
+	//criando metodo statico para usar em outros metodos.
+	public static Usuario criarUsuario() {
+		//ele vai retornar a criacao de um usuario para os metodos que implementarem ele.
+		return Usuario
+				.builder()
+				.nome("jonas")
+				.email("jonas@gmail.com")
+				.senha("1234")
+				.build();	
+	}
+	
 }
