adeee7ac5bbc561e1cbfb0c13f89a4feded0c342
Jonas Lopes <jmdlopes.almeida@gmail.com>
2020-04-27 16:10:14 -0300

    Usando @MockBean para eliminar a implementação manual.

--- src/test/java/com/br/minhasfinancas/services/UsuarioServiceTest.java
+++ src/test/java/com/br/minhasfinancas/services/UsuarioServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,52 +14,55 @@ import com.br.minhasfinancas.repositories.UsuarioRepository;
 import com.br.minhasfinancas.services.exception.RegraNegocioException;
 import com.br.minhasfinancas.services.implementacoes.UsuarioImpService;
 
-
-//@RunWith(SpringRunner.class) com o mock eu posso elimnar essa anotacao.
+@RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
-	
-    //no teste unitatio eu nao vou precisar mais injetar os repositories reais.
+
+	// no teste unitatio eu nao vou precisar mais injetar os repositories reais.
 	UsuarioService service;
+	// criando uma instancia FAKE para melhorar os testes unitários com MOCK. Dessa
+	// forma eu nao preciso executar os metodos reais.
+	@MockBean // usando o mock como um bean gerenciavel pelo framework
 	UsuarioRepository repo;
-	
-	@Before //esse metodo vai iniciar antes dos outros.
+
+	@Before // esse metodo vai iniciar antes dos outros.
 	public void setUp() {
-		//criando uma instancia FAKE para melhorar os testes unitários com MOCK. Dessa forma eu nao preciso executar os metodos reais.
-		repo = Mockito.mock(UsuarioRepository.class);
+
+		// Dessa forma, com o mock gerenciavel eu nao preciso mais de ssa linha de
+		// codigo: repo = Mockito.mock(UsuarioRepository.class);
+
 		service = new UsuarioImpService(repo);
-	
-				
-	}			
-				
-		
-	
-	
-	//@Test para dizer que ele esta esperando uma excecao.
-	//nesse caso quando eu coloco expected = Test.None.class dessa forma eu digo que nao estou esperando nenhuma excecao.
+
+	}
+
+	// @Test para dizer que ele esta esperando uma excecao.
+	// nesse caso quando eu coloco expected = Test.None.class dessa forma eu digo
+	// que nao estou esperando nenhuma excecao.
 	@Test(expected = Test.None.class)
-	public  void deveValidarEmail() {
-		
-		//cenario
-	   //quando eu fizer a chamada do mock repository eu veirifico o email com qualquer string eu vou retornar false
-		//ou seja,  ele vai retornar false quando eu passar qualquer string como parametro para email.
+	public void deveValidarEmail() {
+
+		// cenario
+		// quando eu fizer a chamada do mock repository eu veirifico o email com
+		// qualquer string eu vou retornar false
+		// ou seja, ele vai retornar false quando eu passar qualquer string como
+		// parametro para email.
 		Mockito.when(repo.existsByEmail(Mockito.anyString())).thenReturn(false);
-		
-		//ação
+
+		// ação
 		service.validarEmail("email@email.com");
 
 	}
-	
-	//Aqui eu ja espero que ele lance uma excecao.
+
+	// Aqui eu ja espero que ele lance uma excecao.
 	@Test(expected = RegraNegocioException.class)
 	public void develancarerrAoValidarEmailquandoexistirEmailCadastrado() {
-		
-		//cenario
+
+		// cenario
 		Mockito.when(repo.existsByEmail(Mockito.anyString())).thenReturn(true);
-			
-	     //ação		
+
+		// ação
 		service.validarEmail("email@email.com");
-		
+
 	}
 
 }
