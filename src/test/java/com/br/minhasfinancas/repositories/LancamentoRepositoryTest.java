50d264f19b11db851b904dd45b5aae7c1f1db80d
Jonas Lopes <jmdlopes.almeida@gmail.com>
2020-05-07 16:31:11 -0300

    Testes para lancamentoreposity atualizar e buscar feitos.

--- src/test/java/com/br/minhasfinancas/repositories/LancamentoRepositoryTest.java
+++ src/test/java/com/br/minhasfinancas/repositories/LancamentoRepositoryTest.java
@@ -2,8 +2,11 @@ package com.br.minhasfinancas.repositories;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
+//importacao como static para facilitar a chamada.
+//dessa forma eu nao preciso mais ficar chamando o Assertions.
+import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,62 +29,103 @@ public class LancamentoRepositoryTest {
 
 	@Autowired
 	LancamentoRepository repo;
-	
+
 	@Autowired
 	TestEntityManager entityManager;
-	
-	
+
 	@Test
-	//testando o metodo salvar
-	public  void deveSalvarUmLancamento() {
-		
-		
-		//cenario
+	// testando o metodo salvar
+	public void deveSalvarUmLancamento() {
+
+		// cenario
 		Lancamento lancamento = criarLancamento();
-		//ação
+		// ação
 		lancamento = repo.save(lancamento);
-	
-		//verificação
-		Assertions.assertThat(lancamento.getId()).isNotNull();
-	
+
+		// verificação
+		assertThat(lancamento.getId()).isNotNull();
+
 	}
 
 	@Test
-	//testando o metodo de deletar
+	// testando o metodo de deletar
 	public void deveDeletarUmLancamento() {
-		//cenario
-	    Lancamento lancamento = criarLancamento();	
-		entityManager.persist(lancamento);//Obs: eu poderia usar tb o save. lancamento = repo.save(lancamento);
+		// cenario
+		Lancamento lancamento = criarEPersistirUmLancamento();
 
-		//pegando oo lancamento que eu persist na base.
+		// pegando oo lancamento que eu persist na base.
 		lancamento = entityManager.find(Lancamento.class, lancamento.getId());
-		
-	    //ação
-		//deleto esse lancamento
-	    repo.delete(lancamento);
-	    
-	    //verificão
-	    //primeiro eu vou procurar esse lancamento pelo id
-	    Lancamento lancamentoInexistente = entityManager.find(Lancamento.class, lancamento.getId());
-	  
-	    //então eu verifico se realmente esse lancamento foi deletado.
-	    Assertions.assertThat(lancamentoInexistente).isNull();
-	    
+
+		// ação
+		// deleto esse lancamento
+		repo.delete(lancamento);
+
+		// verificão
+		// primeiro eu vou procurar esse lancamento pelo id
+		Lancamento lancamentoInexistente = entityManager.find(Lancamento.class, lancamento.getId());
+
+		// então eu verifico se realmente esse lancamento foi deletado.
+		assertThat(lancamentoInexistente).isNull();
+
+	}
+
+	// testando o metodo de atualizar
+	@Test
+	public void deveAtualizarUmLancamento() {
+
+		// cenario
+		// adicionando o metodo que cria e salva um lancamento
+		Lancamento lancamento = criarEPersistirUmLancamento();
+
+		// ação
+		// alterando as informacoes deste lancamento
+		lancamento.setAno(2018);
+		lancamento.setDescricao("teste atualizar");
+		lancamento.setMes(2);
+		lancamento.setStatus(StatusLancamento.CANCELADO);
+		// salvando as novas informacoes
+		lancamento = repo.save(lancamento);
+		// buscando o lancamento por id.
+		Lancamento lancamentoatualizado = entityManager.find(Lancamento.class, lancamento.getId());
+
+		// verificação
+		// fazendo a comparacao das novas informacoes para ver se realmente eleas foram
+		// atualizadas
+		assertThat(lancamentoatualizado.getAno()).isEqualTo(2018);
+		assertThat(lancamentoatualizado.getDescricao()).isEqualTo("teste atualizar");
+		assertThat(lancamentoatualizado.getMes()).isEqualTo(2);
+	    assertThat(lancamentoatualizado.getStatus()).isEqualTo(StatusLancamento.CANCELADO);
+
+	}
+
+	// testando o metodo de buscar
+	@Test
+	public void deveBuscarUmLancamentoPorId() {
+
+		// cenario
+		// adicionando o metodo que cria e salva um lancamento
+		Lancamento lancamento = criarEPersistirUmLancamento();
+
+	   //acao
+	   //buscando um lancamento por id.
+	   Optional<Lancamento> lancamentoEncontrado = repo.findById(lancamento.getId());
+	   
+	  //verificacao
+	  //aqui eu verifico se esse lancamento existe como true.
+	  assertThat(lancamentoEncontrado.isPresent()).isTrue();
 	
 	}
-	
+
+	private Lancamento criarEPersistirUmLancamento() {
+		Lancamento lancamento = criarLancamento();
+		entityManager.persist(lancamento);// Obs: eu poderia usar tb o save. lancamento = repo.save(lancamento);
+		return lancamento;
+	}
+
 	private Lancamento criarLancamento() {
-		
-		return Lancamento.builder()
-				.ano(2019)
-				.mes(1)
-				.descricao("lancamento qualquer")
-		        .valor(BigDecimal.valueOf(10))
-		        .tipo(TipoLancamento.RECEITA)
-	            .status(StatusLancamento.PENDENTE)
-	            .dataCadastro(LocalDate.now())
-	            .build();
+
+		return Lancamento.builder().ano(2019).mes(1).descricao("lancamento qualquer").valor(BigDecimal.valueOf(10))
+				.tipo(TipoLancamento.RECEITA).status(StatusLancamento.PENDENTE).dataCadastro(LocalDate.now()).build();
 	}
-	
-    
+
 }
